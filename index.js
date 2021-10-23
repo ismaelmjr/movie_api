ddb21d4a4896db0a0ef2f137c0d7c406f3c4bb86
ismaelmjr <59127089+ismaelmjr@users.noreply.github.com>
2021-10-23 17:49:57 -0500

    REST & API Endpoints

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const express = require('express'),
 
 const app = express();
 
-app.use(morgan('common'));
-app.use(express.static('public'));
+app.use(morgan('common')); //Middelware for logger.
+app.use(express.static('public')); //Middleware for static files.
 
 // Note that app routing is required in any Express application, so you’ll need to define it in your myFlix application. Along with app routing, you’ll also need to define one middleware function for logging your request, and another one for authenticating your users. The order in which you should do so is as follows:
 
@@ -13,72 +13,123 @@ app.use(express.static('public'));
 // App routing
 // Note that app routing is rarely a single function, rather, multiple functions depending on the number of routes you want to specify for your app. You’ll learn more about user authentication later in this Achievement.
 
-// Created a topBooks list.
-let topBooks = [
-  {
-    title: "Harry Potter and the Sorcerer's Stone",
-    author: 'J.K. Rowling',
-  },
-  {
-    title: 'Lord of the Rings',
-    author: 'J.R.R. Tolkien',
-  },
-  {
-    title: 'Twilight',
-    author: 'Stephanie Meyer',
-  },
-];
 //Created a topMovies list.
 let topMovies = [
   {
     title: 'The Shawshank Redemption',
-    year: '1994',
+    genre: ['Prison', 'Drama', 'Crime Fiction'],
+    director: { name: 'Frank Darabot' },
   },
   {
     title: 'The Godfather',
-    year: '1972',
+    genre: ['Mafia', 'Drama', 'Crime Fiction'],
+    director: { name: 'Francis Ford Coppoia' },
   },
   {
     title: 'The Godfather: Part II',
-    year: '1974',
+    genre: ['Mafia', 'Drama', 'Crime Fiction'],
+    director: { name: 'Francis Ford Coppoia' },
   },
   {
     title: '12 Angry Men',
-    year: '1957',
+    genre: ['Drama', 'Trial Drama', 'Crime Film'],
+    director: { name: 'Sidney Lumet' },
   },
   {
     title: 'Schindlers List ',
-    year: '1993',
+    genre: ['War', 'History', 'Black and White'],
+    director: { name: 'Steven Spielberg' },
   },
   {
     title: 'The Lord of the Rings: The Return of the King',
-    year: '2003',
+    genre: ['Novel', 'Fantasy Fiction', 'Adventure Fiction'],
+    director: { name: 'Peter Jackson' },
   },
   {
     title: 'Pulp Fiction',
-    year: '1994',
+    genre: ['Comedy', 'Mafia', 'Crime Film'],
+    director: { name: 'Quentin Tarantino' },
   },
   {
     title: 'The Good, the Bad and the Ugly',
-    year: '1966',
+    genre: ['Spaghetti Western', 'Western', 'Action'],
+    director: { name: 'Sergio Leone' },
   },
   {
     title: 'The Lord of the Rings: The Fellowship of the Ring',
-    year: '2001',
+    genre: ['Novel', 'Fantasy Fiction', 'Adventure Fiction'],
+    director: { name: 'Peter Jackson' },
   },
 ];
 
+// Register New Users.
+let newUsers = [''];
+
 // GET requests
+
+//Get the home page of myFLix App.
 app.get('/', (req, res) => {
   res.send('Welcome to myFlix app!');
 });
 
-app.get('/books', (req, res) => {
-  res.json(topBooks);
+// Get Documentation on Request, URL, and Response formats.
+app.get('/documentation', (req, res) => {
+  res.sendFile('public/documentation.html', { root: __dirname });
 });
 
+// Get a list of all movies to the users.
 app.get('/movies', (req, res) => {
   res.json(topMovies);
+});
+
+// Return data (description, genre, director, image URL, whether it’s featured or not) about a single movie by title to the user.
+app.get('/movies/:title', (req, res) => {
+  res.send(
+    'Successful GET request returning data about a single movies by title to the user.'
+  );
+});
+
+// Return data about a genre (description) by name/title (e.g., “Thriller”).
+app.get('/movies/:title/genre', (req, res) => {
+  res.send('Successful GET request returning data about a genre.');
+});
+
+// Return data about a director (bio, birth year, death year) by name.
+app.get('/movies/director/:name', (req, res) => {
+  res.send('Successful GET request returning data about a director.');
+});
+
+// POST Request.
+
+// Allow new users to register.
+app.post('/newUser', (req, res) => {
+  res.send('Successful POST request user was able to register.');
+});
+
+// Allow users to add a movie to their list of favorites (showing only a text that a movie has been added—more on this later)
+app.post('/newUser/:id/favorites', (req, res) => {
+  res.send('Successful POST request movie has been added to favorites.');
+});
+
+// PUT Request.
+
+// Allow users to update their user info (username, password, email, date of birth).
+app.put('/newUser/:id/info', (req, res) => {
+  res.send('Successful PUT request user info updated.');
+});
+
+// DELETE Request.
+
+// Allow users to remove a movie from their list of favorites (showing only a text that a movie has been removed—more on this later)
+app.delete('/newUser/:id/favorites', (req, res) => {
+  res.send(
+    'Successful DELETE request movie has been deleted from user list of favorites.'
+  );
+});
+
+// Allow existing users to deregister
+app.delete('/newUser', (req, res) => {
+  res.send('Successful DELETE request existing user has been deregistered.');
 });
 
 // listen for requests
